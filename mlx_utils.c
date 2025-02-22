e16f98e9783843696140ed8055fd516863f027bd
Razan Srour <razanmsrour@gmail.com>
2025-02-22 20:47:49 +0300

    creat mlx utils file

---
+++ mlx_utils.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 20:46:35 by rsrour            #+#    #+#             */
+/*   Updated: 2025/02/22 20:46:57 by rsrour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void    ft_mlx_pixel_put(t_data *data, int x_coord, int y_coord, int color)
+{
+    char    *dest;
+    
+    dest = data->addr + (y_coord * data->line_length + 
+            x_coord * (data->bits_per_pixel / 8));
+    *(unsigned int*)dest = color; 
+}
