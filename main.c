23dc302fc98686cf962617ee1a02bc8966391205
Razan Srour <razanmsrour@gmail.com>
2025-02-22 20:40:09 +0300

    create main file

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 19:50:32 by rsrour            #+#    #+#             */
+/*   Updated: 2025/02/22 20:38:01 by rsrour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int     main(int argc, char **argv)
+{
+    t_vars   vars;
+    t_data   img;
+
+    if (argc > 1 && argc < 5) 
+        ft_display_mandel(argv);
+    else
+    {
+        perror("Usage: COMPLETE THIS LATER");
+        exit(1);
+    }
+    return (0);
+}
