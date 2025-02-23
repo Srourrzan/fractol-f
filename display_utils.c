d092a88b84eed7fd4bc1454a8c9a5f3004e24a1d
Razan Srour <razanmsrour@gmail.com>
2025-02-23 20:35:44 +0300

    handle double input

--- display_utils.c
+++ display_utils.c
@@ -6,12 +6,28 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 20:50:44 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/22 20:51:27 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/23 20:32:43 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	ft_display_fractals(char **argv)
+void	ft_display_fractals(char **argv, int argc)
 {
+    if (!ft_validate_args(argv))
+        ft_exit(2);
+    if (!ft_strcmp(argv[1], "Mandelbrot"))
+    {
+        ft_mandelbrot();
+    }    
+    else if (!ft_strcmp(argv[1], "Julia") && (argc == 4 || argc == 2))
+    {
+        
+    }
+    else if (!ft_strcmp(argv[1], "Sierpinski"))
+    {
+        
+    }
+    else
+        ft_exit(1);
 }
--- std_utils.c
+++ std_utils.c
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 20:25:37 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/22 20:30:07 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/23 20:32:09 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,102 @@ int	ft_strcmp(const char *s1, const char *s2)
 		iter++;
 	return (s1[iter] - s2[iter]);
 }
+
+void	ft_exit(int exit_num)
+{
+	exit(exit_num);
+}
+// int	ft_atoi(char *src)
+// {
+// 	int	iter;
+// }
+
+int		ft_is_float(char *src)
+{
+	int		iter;
+	int		status;
+	int		d_check;	
+	
+	iter = 0;
+	status = 1;
+	d_check = 0;
+	while ((src[iter] >= '0' && src[iter] <= '9') 
+			|| (src[iter] == '.') || (src[iter] == '-') 
+			|| (src[iter] == '+'))
+	{
+		if ((src[iter] == '-' || src[iter] == '+') && iter > 0)
+		{
+			status = 0;
+			break;
+		}
+		if (src[iter] == '.')
+		{
+			d_check += 1;
+			if (d_check > 1)
+			{
+				status = 0;
+				break;
+			}
+		}
+		iter++;
+	}
+	return (status);
+}
+
+double	ft_atof(char *src)
+{
+	int	iter;
+	double	d_num;
+	int		ten;
+	int		sign;
+	
+	sign = 1;
+	d_num = 0.0;
+	iter = 0;
+	ten = 10;
+	if (src[iter] == '-' || src[iter] == '+')
+	{
+		if (src[iter] == '-')
+			sign = -1;
+		iter++;
+	}
+	while ((src[iter] >= '0' && src[iter] <= '9') || (src[iter] == '.'))
+	{
+		if (src[iter] == '.')
+		{
+			iter++;
+			break;
+		}
+		d_num = (d_num * 10) + (src[iter] - '0');
+		iter++;
+	}
+	while (src[iter] >= '0' && src[iter] <= '9')
+	{
+		d_num = (src[iter] - '0') / ten;
+		ten *= 10;
+		iter++;
+	}
+	return (d_num * sign);
+}
+
+int		ft_validate_args(char **argv)
+{
+	int		iter;
+
+	iter = 2;
+	while (argv[iter])
+	{
+		if(!ft_is_float(argv[iter]))
+			return (0);
+		iter++;
+	}
+	return (1);
+}
+
+// int	main()
+// {
+// 	char *s1 = "9.8";
+	
+// 	printf("s1 = %f\n", ft_atof(s1));
+// 	return(0);
+// }
