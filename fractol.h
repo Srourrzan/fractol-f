261c9b523db412e9ff7f5bc370da40c6a3714e8d
Razan Srour <razanmsrour@gmail.com>
2025-02-24 20:00:22 +0300

    find the coeff of the mandelbrot set

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 17:59:41 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/24 19:35:25 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/24 19:58:35 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-# define CX 0
-# define CY 0.8
+#define CX 0
+#define CY 0.8
 
 typedef struct s_image
 {
@@ -33,6 +33,8 @@ typedef struct s_fractal
 	double		cimaginary;
 	double		minreal;
 	double		maxreal;
+	double		min_im;
+	double		max_im;
 	int			x;
 	int			y;
 	int			zoom;
@@ -51,27 +53,29 @@ typedef struct s_mlx
 // std functions
 int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strlen(char *src);
-char	*ft_strchr(char *src, int c);
-char	*ft_strrchr(char *src, int c);
+char			*ft_strchr(char *src, int c);
+char			*ft_strrchr(char *src, int c);
 
 // mlx functions
 void			ft_mlx_pixel_put(t_image *data, int x_coord, int y_coord,
 					int color);
-void	ft_init_mlx(t_mlx *i_mlx);
+void			ft_init_mlx(t_mlx *i_mlx);
 
 // sets functions
 void			ft_init_by_id(t_mlx *i_mlx, char **argv, int argc);
-void   ft_fractol(char **argv, int argc);
+void			ft_fractol(char **argv, int argc);
 
 // finish_utils functions
 void			ft_exit(int exit_num);
-void	ft_clean(t_mlx *i_mlx);
-void 	ft_clean_and_exit(t_mlx *i_mlx, int exit_num);
+void			ft_clean(t_mlx *i_mlx);
+void			ft_clean_and_exit(t_mlx *i_mlx, int exit_num);
 
-//parse_args functions
+// parse_args functions
 int				ft_is_float(char *src);
 int				ft_validate_args(char **argv);
-double	ft_atof(char *src);
+double			ft_atof(char *src);
 
-//mandelbrot functions
-void ft_mandelbrot(t_mlx *i_mlx);
+// mandelbrot functions
+void			ft_mandelbrot(t_mlx *i_mlx);
+void			ft_coeffs(t_fractal *fractal);
+void			ft_init_mandelbrot(t_fractal *fractal);
--- mandelbrot.c
+++ mandelbrot.c
@@ -6,50 +6,52 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 16:32:36 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/24 19:35:57 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/24 19:58:39 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-//
-void  ft_init_mandelbrot(t_fractal *fractal)
+void	ft_init_mandelbrot(t_fractal *fractal)
 {
-    fractal->x = 100;
-    fractal->y = 100;
-    fractal->height = 1000;
-    fractal->width = 1000;
-    fractal->minreal = -3.0;
-    fractal->maxreal = 2.0;
+	fractal->x = 100;
+	fractal->y = 100;
+	fractal->height = 1000;
+	fractal->width = 1000;
+	fractal->minreal = -3.0;
+	fractal->maxreal = 2.0;
 }
 
-double  ft_real(t_fractal fractal)
+void	ft_coeffs(t_fractal *fractal)
 {
-    double  range;
+	double	range;
 
-    range = fractal.maxreal - fractal.minreal;
-    return (fractal.x * (range / fractal.width) + fractal.minreal);
+	range = fractal->maxreal - fractal->minreal;
+	fractal->creal = fractal->x * (range / fractal->width) + fractal->minreal;
+	range = fractal->max_im - fractal->min_im;
+	fractal->cimaginary = fractal->y * (range / fractal->height)
+		+ fractal->min_im;
 }
 /*
  * z = x + iy
-*/
-void ft_mandelbrot(t_mlx *i_mlx)
+ */
+void	ft_mandelbrot(t_mlx *i_mlx)
 {
-    t_fractal fractal;
-    
-    fractal = i_mlx->fractal;
-    ft_init_mandelbrot(&fractal);
-    mlx_string_put(i_mlx->mlx, i_mlx->win, 450, 10, 0xfefefe ,"Mandelbrot");
-    while (fractal.x < fractal.width)
-    {
-        fractal.y = 100;
-        while (fractal.y < fractal.height)
-        {
-            fractal.creal = ft_real(fractal);
-            printf("%f\n", fractal.creal);
-            // ft_mlx_pixel_put(&i_mlx->img.img, fractal.creal)
-            fractal.y++;
-        }
-        fractal.x++;
-    }
+	t_fractal fractal;
+
+	fractal = i_mlx->fractal;
+	ft_init_mandelbrot(&fractal);
+	mlx_string_put(i_mlx->mlx, i_mlx->win, 450, 10, 0xfefefe, "Mandelbrot");
+	while (fractal.x < fractal.width)
+	{
+		fractal.y = 100;
+		while (fractal.y < fractal.height)
+		{
+			ft_coeffs(&fractal);
+			// ft_mlx_pixel_put(&i_mlx->img, fractal.creal, fractal.cimaginary,
+				0xE5989B);
+			fractal.y++;
+		}
+		fractal.x++;
+	}
 }
